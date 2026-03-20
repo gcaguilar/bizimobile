7325c56cb55e6b7eb01ff560a7d160b6b53d3993
gcaguilar <38892827+gcaguilar@users.noreply.github.com>
2026-03-20 15:25:36 +0100

    Fallback Apple shared locale to Spanish

--- shared/core/src/appleMain/kotlin/com/gcaguilar/biciradar/core/Localization.apple.kt
+++ shared/core/src/appleMain/kotlin/com/gcaguilar/biciradar/core/Localization.apple.kt
@@ -1,14 +1,3 @@
 package com.gcaguilar.biciradar.core
 
-import platform.Foundation.NSLocale
-
-actual fun currentAppLanguage(): AppLanguage {
-  val code = NSLocale.currentLocale.languageCode?.lowercase()
-  return when (code) {
-    "en" -> AppLanguage.EN
-    "ca" -> AppLanguage.CA
-    "eu" -> AppLanguage.EU
-    "gl" -> AppLanguage.GL
-    else -> AppLanguage.ES
-  }
-}
+actual fun currentAppLanguage(): AppLanguage = AppLanguage.ES
