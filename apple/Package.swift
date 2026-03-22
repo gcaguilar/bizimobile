2cf7121f1eb280dcf78020e90ffab63cad618886
gcaguilar <38892827+gcaguilar@users.noreply.github.com>
2026-03-22 19:11:36 +0100

    ci: add Package.swift for Dependabot SPM support

---
+++ apple/Package.swift
@@ -0,0 +1,10 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "BiciRadar",
+    platforms: [.iOS(.v12), .watchOS(.v9)],
+    products: [],
+    dependencies: [],
+    targets: []
+)
