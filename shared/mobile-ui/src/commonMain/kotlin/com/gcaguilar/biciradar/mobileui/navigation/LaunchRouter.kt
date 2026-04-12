e6abc8d209db722dc81fa93c7356e6eaf68fda49
gcaguilar <38892827+gcaguilar@users.noreply.github.com>
2026-04-12 23:40:14 +0200

    fix(ktlint): sort imports lexicographically in LaunchRouter.kt

--- shared/mobile-ui/src/commonMain/kotlin/com/gcaguilar/biciradar/mobileui/navigation/LaunchRouter.kt
+++ shared/mobile-ui/src/commonMain/kotlin/com/gcaguilar/biciradar/mobileui/navigation/LaunchRouter.kt
@@ -1,9 +1,9 @@
 package com.gcaguilar.biciradar.mobileui.navigation
 
 import androidx.navigation.NavHostController
-import com.gcaguilar.biciradar.mobileui.state.AppState
 import com.gcaguilar.biciradar.mobileui.LaunchCoordinator
 import com.gcaguilar.biciradar.mobileui.LaunchResolution
+import com.gcaguilar.biciradar.mobileui.state.AppState
 
 /**
  * Sealed interface representing launch requests from mobile intents.
