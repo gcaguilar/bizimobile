24903f1c27a59f6b018735593c67d4492ce7f177
gcaguilar <38892827+gcaguilar@users.noreply.github.com>
2026-04-25 13:53:07 +0200

    Remove unused root mobile services plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,6 @@ plugins {
   alias(libs.plugins.android.kotlin.multiplatform.library) apply false
   alias(libs.plugins.compose.compiler) apply false
   alias(libs.plugins.compose.multiplatform) apply false
-  alias(libs.plugins.firebase.crashlytics) apply false
-  alias(libs.plugins.google.services) apply false
   alias(libs.plugins.kotlin.jvm) apply false
   alias(libs.plugins.kotlin.multiplatform) apply false
   alias(libs.plugins.kotlin.serialization) apply false
